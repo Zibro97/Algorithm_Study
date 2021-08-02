0423418ca9999abfe01eea0353441e0d11149f1c
Zibro <jgh6272@naver.com>
2021-08-02 23:05:24 +0900

    단어의 개수

---
+++ BOJ/7단계/1152.kt
@@ -0,0 +1,9 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    print("${StringTokenizer(br.readLine()).countTokens()}")
+}
