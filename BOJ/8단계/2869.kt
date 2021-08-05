500be601f7226c249cefc0f930cf3dba0219a137
Zibro <jgh6272@naver.com>
2021-08-05 23:03:20 +0900

    달팽이는 올라가고싶다

--- BOJ/8단계/2869.kt
+++ BOJ/8단계/2869.kt
@@ -1,3 +1,5 @@
+//내가 풀어서 시간초과 된 문제
+//시간 0.25초 제한
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.*
@@ -22,5 +24,27 @@ fun main() {
     }
 
     print("${day}")
+
+}
+// 정답
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val input = StringTokenizer(br.readLine())
+
+    val a = input.nextToken().toInt()//낮에 A미터 올라감
+    val b = input.nextToken().toInt()//밤에 B미터 미끄러짐
+    val move = a-b //마지막 날을 제외한 하루에 움직이는 거리
+    var v = input.nextToken().toInt()//v미터인 나무막대
+    val remain = v - a //
+    
+    if(remain % move == 0){
+        print("${remain/move+1}")
+    }else{
+        print("${remain/move+2}")
+    }
 
 }
