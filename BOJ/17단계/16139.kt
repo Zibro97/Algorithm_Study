4557221415b4e3cc34bb7a6421cd42599f77acca
Zibro97 <jgh6272@naver.com>
2022-06-09 00:08:56 +0900

    인간-컴퓨터 상호작용 (100점) 수정

--- BOJ/17단계/16139.kt
+++ BOJ/17단계/16139.kt
@@ -1,13 +1,26 @@
-fun main() {
-    val s = StringBuilder(readLine())
-    val q = readLine()!!.toInt()
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
-    repeat(q){
-        val (a,l,r) = readLine()!!.split(" ")
-        var cnt =0
-        for(i in l.toInt()..r.toInt()){
-           if(s[i] == a[0])  cnt++
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
+    val s = StringBuilder(readLine())
+    val sum = Array(s.length){ IntArray(26) }
+    val q = readLine().toInt()
+    val sb = StringBuilder()
+
+    sum[0][s[0] - 'a']++
+    for(i in 1 until s.length){
+        val tmp = s[i] - 'a'
+        repeat(26){ j->
+            sum[i][j] = sum[i-1][j]
         }
-        println(cnt)
+        sum[i][tmp]++
     }
+    repeat(q){
+        val (a,l,r) = readLine().split(" ")
+
+        if(l.toInt() == 0) sb.append(sum[r.toInt()][a[0] - 'a']).append('\n')
+        else sb.append(sum[r.toInt()][a[0]-'a'] - sum[l.toInt()-1][a[0]-'a']).append('\n')
+    }
+    println(sb)
+    close()
 }
